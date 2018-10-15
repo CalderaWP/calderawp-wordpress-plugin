2fc9ea8fa3cbcf593673e9e0c1461c7571285254
Josh Pollock <josh@calderawp.com>
2018-10-15 15:17:04 -0400

    migrate test X3 Calculations create@1.5.5 dev@1.5.6.2

--- cypress/integration/calculations-x2.test.js
+++ cypress/integration/calculations-x2.test.js
@@ -21,19 +21,30 @@ describe('Name of test', () => {
 		visitPage('x2-calculations-create1-5-5-dev1-5-6-2');
 	});
 
-	const formId = 'cf111';
-
+	const formId = 'CF59dd5d8e95ffb';
+	const totalCalc = 'fld_6617658';
+	const option1Select = 'fld_8172473';
+	const option2Select = 'fld_8186270';
 
 	function testInitialLoad() {
-		//Define how it loads here
+		cfFieldGetCalcFieldValueIs(totalCalc, '0.00');
+
 	}
 
 	it( 'Has the correct initial load', () => {
 		testInitialLoad();
 	});
-	it( 'Does something else', () => {
-		testInitialLoad();
-		//test form
-	});
 
+	it( 'Updates and does math correctly', () => {
+		testInitialLoad();
+
+		cfFieldSelectValue(option1Select, '4' );
+		cfFieldGetCalcFieldValueIs(totalCalc, '4.28');
+
+		cfFieldSelectValue(option2Select, 'Two' );
+		cfFieldGetCalcFieldValueIs(totalCalc, '6.42');
+
+		cfFieldSelectValue(option1Select, '3' );
+		cfFieldGetCalcFieldValueIs(totalCalc, '5.35');
+	});
 });
