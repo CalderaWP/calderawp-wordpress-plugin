1a87ffcd1af4507ef691de1429aa7c5ad85f74ec
Josh Pollock <josh@calderawp.com>
2018-11-09 16:27:06 -0500

    tests for entry viewer component

--- src/components/EntryViewer/EntryHeader.js
+++ src/components/EntryViewer/EntryHeader.js
@@ -4,10 +4,10 @@ import React, {Fragment} from 'react';
 import type {EntryField} from "../../flow-types/entryTypes";
 export const EntryHeader = (props: {
 	entryField: EntryField,
-	labelWith: string
+	labelWith?: string
 } ) => {
 	const {entryField,labelWith} = props;
-	const label = labelWith && labelWith.length ? labelWith : entryField.slug
+	const label = labelWith && labelWith.length ? labelWith : entryField.slug;
 	return (
 		<Fragment>
 			{label}
