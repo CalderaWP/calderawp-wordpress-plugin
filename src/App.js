931e7d5feb914e59b0fc11b25cf1c1017d8b6c71
Josh Pollock <josh@calderawp.com>
2018-12-04 13:58:27 -0500

    make the css not fucking terrible

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 //import { BrowserRouter as Router, Link } from "react-router-dom";
-import React from 'react';
+import React, {Fragment} from 'react';
 import Entries from "./Pages/Entries";
+import "./css/caldera-css/admin.css";
+import "./css/guten-css/components/style.css";
+import "./css/guten-css/edit-post/style.css";
+import "./css/guten-css/editor/style.css";
 import "./app.css";
 import {LogoutButton} from "./components/Login/LogoutButton";
 import {JwtLogin} from "./components/Login/JwtLogin";
+import {Admin} from '@caldera-labs/components'
+import { NavigableMenu, TabbableContainer, Button } from '@wordpress/components';
+import logo from './globe.svg';
+
+const MainMenu = ({onLogout,isLoggedIn}) => (
+	<div className={'edit-post-header-toolbar'}>
+		<NavigableMenu orientation="horizontal">
+			{isLoggedIn &&
+				<LogoutButton className={'caldera-pro-logout'} onLogout={onLogout}/>
+			}
+		</NavigableMenu>
+	</div>
+);
+
 
 class App extends React.Component {
-
-
-
 	render() {
 		const {
 			getForms,
@@ -19,23 +34,35 @@ class App extends React.Component {
 			isLoggedIn
 		} = this.props;
 
-
 		return(
-			<div>
-				{isLoggedIn ? (
-					<div>
-						<LogoutButton onLogout={onLogout}/>
-						<Entries
-							getForms={getForms}
-							getEntries={getEntries}
-						/>
-					</div>
-
-				) : (
-					<JwtLogin
-						onTokenReceived={onJwtToken}
+			<div id="editor" className="gutenberg__editor caldera-pro-editor">
+				<div id={'caldera-pro-header'}>
+					<img src={logo} height={'40px'} width={'40px'} />
+					<h1 className="App-title">Caldera Pro </h1>
+					<MainMenu
+						onLogout={onLogout}
+						isLoggedIn={isLoggedIn}
 					/>
-				)}
+				</div>
+				<div id={'caldera-pro-body'}>
+						<div className={'caldera-pro-inner'}>
+							{isLoggedIn ? (
+							<Fragment>
+								<Entries
+									getForms={getForms}
+									getEntries={getEntries}
+								/>
+							</Fragment>
+
+						) : (
+							<JwtLogin
+								onTokenReceived={onJwtToken}
+							/>
+						)}
+					</div>
+				</div>
+
+
 			</div>
 		)
 
@@ -43,3 +70,5 @@ class App extends React.Component {
 }
 
 export default App;
+
+
